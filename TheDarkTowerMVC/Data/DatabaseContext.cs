e66f1fd73dafeb58228b437fc5f500f00e87aba9
Ioana Anton <Anton.Co.Ioana@student.utcluj.ro>
2022-03-31 12:19:01 +0300

    Add project files.

---
+++ TheDarkTowerMVC/Data/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TheDarkTowerMVC.Data
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        //comenzi consola pentru migrations
+        // dotnet ef migrations add Classes
+        // dotnet ef database drop
+        // dotnet ef database update
+
+        //definirea tabelelor
+
+
+    }
+}
---
+++ TheDarkTowerMVC/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿namespace TheDarkTowerMVC.Entity
+{
+    public class User
+    {
+        //[Key]
+        public String Id { get; set; } = Guid.NewGuid().ToString();
+
+        public String Name { get; set; }
+
+        //[Required]
+        public String Email { get; set; }
+
+        //[Required]
+        public String Password { get; set; }
+    }
+}
